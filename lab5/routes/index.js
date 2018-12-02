41693d29fac65a3cc7b81122317f9ab7020389f9
kos602 <kalmurzaev.oljas@gmail.com>
2018-12-02 16:14:21 -0600

    lab 5

--- lab5/routes/index.js
+++ lab5/routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.set('Cache-Control', 'private, max-age=86400')
+    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
--- lab5/routes/users.js
+++ lab5/routes/users.js
@@ -1,10 +1,17 @@
 var express = require('express');
+var axios = require('axios');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.set('Cache-Control', 'public, max-age=360');
-  res.send('respond with a resource');
+    res.set('Cache-Control', 'private, max-age=86400');
+    new Promise(function (resolve, reject) {
+        const data = axios.get('https://randomuser.me/api/', {params: {results: 10}});
+        if (data) {
+            resolve(data)
+        }
+    }).then(res.json(data));
+
 });
 
 module.exports = router;
